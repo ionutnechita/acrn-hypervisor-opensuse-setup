f7cc158d48d1b07b0479c2609b5f88beb4bac4c6
Ionut Nechita <ionut_n2001@yahoo.com>
2021-02-13 12:32:19 +0200

    Welcome message added in project
    Description: - VERSION: 0.1.2-DEV
     - NAME: Project ACRN Embedded Hypervisor using OpenSUSE OS - Setup Steps - General Purpose Flavor
     - LICENSE: BSD 3-Clause "New" or "Revised" License
     - CODEOWNERS: first code owner - @ionutnechita
     - > Welcome on $NAME
     - Tags: LICENSE, BSD, SPDX, GO, main
    
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>
    Change-Id: I441ab99090f4aa01af3fd354d7fbd3123b58f5f9

---
+++ main.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2021, Project ACRN Embedded Hypervisor using OpenSUSE OS - Setup Steps, Ionut Nechita.
+// All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome on Project ACRN Embedded Hypervisor using OpenSUSE OS - Setup Steps")
+}
